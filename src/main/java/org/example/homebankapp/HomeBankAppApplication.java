604cbc9e17e4070431b0c9a5a54c032282e7c95a
elena <kronverk01@gmail.com>
2026-07-22 12:31:10 +0100

    first commit

---
+++ src/main/java/org/example/homebankapp/HomeBankAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.homebankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBankAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeBankAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/homebankapp/HomeBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.homebankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
