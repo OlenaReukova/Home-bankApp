4e0e8b366dd3d081b0192ebe4faf53819ed705c7
elena <kronverk01@gmail.com>
2026-07-22 13:16:18 +0100

    002: Bank Model

---
+++ src/main/java/org/example/homebankapp/model/Bank.java
@@ -0,0 +1,10 @@
+package org.example.homebankapp.model;
+
+import java.math.BigDecimal;
+
+public class Bank {
+    private String id;
+    private String firstName;
+    private String secondName;
+    private BigDecimal balance;
+}
