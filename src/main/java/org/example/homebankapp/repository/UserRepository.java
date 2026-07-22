83826cf5c3c91e59bba4bb723bada0339f4cbfba
elena <kronverk01@gmail.com>
2026-07-22 15:24:25 +0100

    005: UserRepository: @Repository - An annotation for indicating this class is or storage, retrieval, update, delete and search on objects. extends MongoRepository allows BookRepository to inherit CRUD methods e.g. findById(), save(), delete()

---
+++ src/main/java/org/example/homebankapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.homebankapp.repository;
+
+import org.example.homebankapp.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
