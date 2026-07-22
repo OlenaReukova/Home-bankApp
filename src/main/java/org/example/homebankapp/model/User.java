25d59019f321f8a6820de00b64c3a0deea54afe1
elena <kronverk01@gmail.com>
2026-07-22 13:19:58 +0100

    002: User Model

---
+++ src/main/java/org/example/homebankapp/model/User.java
@@ -0,0 +1,19 @@
+package org.example.homebankapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String firstName;
+    private String secondName;
+    private BigDecimal balance;
+}
