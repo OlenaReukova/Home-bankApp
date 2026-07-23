693139f9cac64afee20dc03ab317042329561e13
olena.reukova <kronverk01@gmail.com>
2026-07-23 13:04:48 +0100

    007: Add ResponseEntity to the POST request

--- src/main/java/org/example/homebankapp/controller/ApplicationController.java
+++ src/main/java/org/example/homebankapp/controller/ApplicationController.java
@@ -3,6 +3,8 @@ package org.example.homebankapp.controller;
 import lombok.AllArgsConstructor;
 import org.example.homebankapp.dto.UserDto;
 import org.example.homebankapp.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +20,10 @@ public class ApplicationController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public UserDto createUser(@RequestBody UserDto userDto){
-        return userService.create(userDto);
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+        UserDto createdUser = userService.create(userDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(createdUser);
     }
 }
