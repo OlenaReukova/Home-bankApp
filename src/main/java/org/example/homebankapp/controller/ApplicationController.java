83123acddd9eda73459c1d675460263e69977d1d
olena.reukova <kronverk01@gmail.com>
2026-07-23 12:57:06 +0100

    007: Add POST api to create User

--- src/main/java/org/example/homebankapp/controller/ApplicationController.java
+++ src/main/java/org/example/homebankapp/controller/ApplicationController.java
@@ -1,10 +1,24 @@
 package org.example.homebankapp.controller;
 
+import lombok.AllArgsConstructor;
+import org.example.homebankapp.dto.UserDto;
+import org.example.homebankapp.service.UserService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping()
+@RequestMapping("/bank")
 @RestController
+@AllArgsConstructor
+@Validated
 public class ApplicationController {
 
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public UserDto createUser(@RequestBody UserDto userDto){
+        return userService.create(userDto);
+    }
 }
