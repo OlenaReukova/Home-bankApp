a26dabde4927940c58da8308b0a3383ab1978249
olena.reukova <kronverk01@gmail.com>
2026-07-28 16:19:29 +0100

    009: Add GET endpoint for getAllUsers

--- src/main/java/org/example/homebankapp/controller/ApplicationController.java
+++ src/main/java/org/example/homebankapp/controller/ApplicationController.java
@@ -7,10 +7,9 @@ import org.example.homebankapp.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/bank")
 @RestController
@@ -21,10 +20,16 @@ public class ApplicationController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public ResponseEntity<UserDto> createUser(@Valid  @RequestBody UserDto userDto){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
         UserDto createdUser = userService.create(userDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(createdUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
     }
 }
