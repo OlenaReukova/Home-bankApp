5c2900227e6edd77d0c63c1e86cd4d6ee446300b
olena.reukova <kronverk01@gmail.com>
2026-07-30 12:40:19 +0100

    013: Add partialUpdateUser method to the UserService

--- src/main/java/org/example/homebankapp/service/UserService.java
+++ src/main/java/org/example/homebankapp/service/UserService.java
@@ -1,5 +1,6 @@
 package org.example.homebankapp.service;
 
+import org.example.homebankapp.dto.UpdateUserRequest;
 import org.example.homebankapp.dto.UserDto;
 import org.example.homebankapp.dto.UserMapper;
 import org.example.homebankapp.exception.UserNotFoundException;
@@ -44,5 +45,26 @@ public class UserService {
         User updateUser = userRepository.save(existingUser);
         return userMapper.toDto(updateUser);
 
+    }
+
+    public UserDto partialUpdateUser(String id, UpdateUserRequest request) {
+        User existingUser = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+
+        if (request.firstName() != null && !request.firstName().isBlank()) {
+            existingUser.setFirstName(request.firstName());
+        }
+        if (request.lastName() != null && !request.lastName().isBlank()) {
+            existingUser.setLastName(request.lastName());
+        }
+
+        if (request.email() != null && !request.email().isBlank()) {
+            existingUser.setEmail(request.email());
+        }
+
+        if (request.phoneNumber() != null && !request.phoneNumber().isBlank()) {
+            existingUser.setPhoneNumber(request.phoneNumber());
+        }
+
+        return userMapper.toDto(userRepository.save(existingUser));
     }
 }
